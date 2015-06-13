0523286b1c1699b312b69eaccfaacab9c1b8ada3
missling <ling.giang@gmail.com>
2015-06-13 13:33:26 -0700

    create user signup page

--- roadtripper/app/controllers/users_controller.rb
+++ roadtripper/app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      username: params[:user][:username],
+      email: params[:user][:email],
+      password: params[:user][:password]
+      )
+
+      if params[:user][:password] == params[:user][:password_confirmation] 
+        @user.save
+        session[:user_id] = user.id
+        redirect_to user_path
+      else
+        @error = "Password confirmation must match Password"
+        render :new
+      end
+  end
 end
